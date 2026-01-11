50780c889ca7bf6160a18111ab77f9944bc02040
dhivyapraban <dhivyaprabanrk@gmail.com>
2026-01-11 09:12:24 +0530

    Fix: Cors issue fixed

--- index.js
+++ index.js
@@ -16,8 +16,12 @@ app.use(express.json());
 // CORS Middleware
 app.use(cors({
   origin: process.env.FRONTEND_URL,
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.options("*", cors());
 
 // Connect to MongoDB
 connectDB();
