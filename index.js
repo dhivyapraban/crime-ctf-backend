3f651ecbc6d4f3ee027bf017e1e19d831ed49f74
dhivyapraban <dhivyaprabanrk@gmail.com>
2026-01-11 08:16:21 +0530

    Fix: Removed static cors

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // CORS Middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 
 // Connect to MongoDB
 connectDB();
