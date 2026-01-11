fc9061a73eaa7b969748900106233fbbdad0fe93
dhivyapraban <dhivyaprabanrk@gmail.com>
2026-01-11 08:51:08 +0530

    Feat: Added CORS

--- index.js
+++ index.js
@@ -13,15 +13,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // CORS Middleware
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   if (req.method === 'OPTIONS') {
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
+app.use(cors({
+  origin: process.env.API_URL,
+  credentials: true
+}));
 
 // Connect to MongoDB
 connectDB();
